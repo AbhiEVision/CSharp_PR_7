a75a4264106f572f4c13281515734ac6307ebdda
Abhi Patel <abhi.d@simformsolutions.com>
2023-05-11 10:31:28 +0530

    add : OCP violated example

---
+++ CSharp_PR_7/OCP/Violated/BankAccount.cs
@@ -0,0 +1,33 @@
+﻿namespace CSharp_PR_7.OCP.Violated
+{
+	internal class BankAccount
+	{
+		private string _userName;
+		private string _password;
+		public AccountType accountType { get; init; }
+
+		public string Account { get { return _userName; } set { _userName = value; } }
+		public string Password { get { return _password; } set { _password = value; } }
+
+		public BankAccount(string account, string password, AccountType accountType)
+		{
+			Account = account;
+			Password = password;
+			this.accountType = accountType;
+		}
+
+		public double CalculateIntrust(double money)
+		{
+			if (accountType == AccountType.CurrentAccount)
+			{
+				return money * 0.8;
+			}
+			else if (accountType == AccountType.SavingAccount)
+			{
+				return money * 0.5;
+			}
+
+			return money;
+		}
+	}
+}
---
+++ CSharp_PR_7/OCP/Violated/Program.cs
@@ -0,0 +1,19 @@
+﻿// OCP said that if class are made that it must be open to extend but close to modify. means if we add new functionality that current
+// class's don't be updated!
+
+// Here OCP is violated because if i want to add an one more type of account like boss said to we want to add a SalaryAccount
+// than i have to add that type in enum ( AccountType ) and also i have to update the CalculateIntrust Method inside a BankAccount.
+
+
+namespace CSharp_PR_7.OCP.Violated
+{
+	internal class Program
+	{
+		public static void Main(string[] args)
+		{
+			BankAccount account = new("Abhi Patel", "Abhi@123", AccountType.CurrentAccount);
+
+			Console.WriteLine(account.CalculateIntrust(12334.12332));
+		}
+	}
+}
